9047551b22b3faaf1d7d457ba05eaf2aca3bae76
N0rR <n0rr7882@gmail.com>
2018-03-17 02:05:48 +0900

    post card 디자인 어느정도 완성된 듯 하다

--- src/app/devlog-routing.module.ts
+++ src/app/devlog-routing.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
 import { FollowsComponent } from './follows/follows.component';
 import { SearchComponent } from './search/search.component';
-import { MypageComponent } from './mypage/mypage.component'
+import { MypageComponent } from './mypage/mypage.component';
 
 const routes: Routes = [
     { path: '', redirectTo: 'timeline', pathMatch: 'full' },
