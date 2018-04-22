b2ed0af547e503ab4f14f8acd6e51443c65d6920
N0rR <n0rr7882@gmail.com>
2018-04-22 17:04:52 +0900

    DB url 변경 (db이전)

--- server/config/constants.js
+++ server/config/constants.js
@@ -1,7 +1,8 @@
 export default {
     PORT: 3000,
     LOG_FORMAT: 'dev',
-    DB_URL: 'mongodb://localhost/vaccine',
-    // DB_URL: 'mongodb://admin:Asdf!234@ds233769.mlab.com:33769/vaccine-dev',
+    // DB_URL: 'mongodb://localhost/vaccine',
+    // DB_URL: `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@ds233769.mlab.com:33769/vaccine-dev`,
+    DB_URL: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@vaccine-dev-hvf7h.mongodb.net/test`,
     JWT_SALT: '~!@#$%^&*()_+Vaccine+_)(*&^%$#@!~'
 };
